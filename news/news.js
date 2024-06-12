f7b12b816e1472e23e795b8f72ff1a79614d882c
HansikYun <150501208+HansikYun@users.noreply.github.com>
2024-06-12 16:39:49 +0900

    Add files via upload

--- news/news.js
+++ news/news.js
@@ -4,7 +4,7 @@ const apiKey = '44b945b9d66e44edb34a121a0536a76e';
 async function fetchKoreaNews() {
     const response = await fetch(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`);
     const data = await response.json();
-    return data.articles.slice(0, 2); // Get only first two articles
+    return data.articles;
 }
 
 // Function to display news on the webpage
@@ -12,13 +12,7 @@ async function displayNews() {
     const newsContainer = document.getElementById('news');
     const articles = await fetchKoreaNews();
 
-    let currentIndex = 0; // Variable to keep track of the current index of displayed articles
-
-    function showNextArticle() {
-        newsContainer.innerHTML = ''; // Clear previous articles
-
-        const article = articles[currentIndex];
-
+    articles.forEach(article => {
         const newsItem = document.createElement('div');
         newsItem.classList.add('news-item');
 
@@ -31,10 +25,7 @@ async function displayNews() {
         content.classList.add('news-item-content');
 
         const title = document.createElement('h2');
-        const link = document.createElement('a'); // Link element
-        link.href = article.url; // URL of the article
-        link.textContent = article.title;
-        title.appendChild(link); // Adding link as a child of title
+        title.textContent = article.title;
         content.appendChild(title);
 
         const description = document.createElement('p');
@@ -44,16 +35,7 @@ async function displayNews() {
         newsItem.appendChild(content);
 
         newsContainer.appendChild(newsItem);
-
-        // Increment currentIndex and handle wrapping around
-        currentIndex = (currentIndex + 1) % articles.length;
-    }
-
-    // Display first article immediately
-    showNextArticle();
-
-    // Set interval to show next article every 5 seconds
-    setInterval(showNextArticle, 5000);
+    });
 }
 
 // Display news when the page loads
